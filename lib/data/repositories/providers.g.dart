90d95fda933b23d6665353aa8548119f64196f51
John Ivan Puayap <johnivanpuayap@gmail.com>
2026-05-16 17:01:37 +0800

    feat(data): register repositories with get_it + expose Riverpod providers

---
+++ lib/data/repositories/providers.g.dart
@@ -0,0 +1,113 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authRepositoryHash() => r'0fd5d6710fcd0cdb55ff0e7490ffe0a82520069a';
+
+/// See also [authRepository].
+@ProviderFor(authRepository)
+final authRepositoryProvider = Provider<AuthRepository>.internal(
+  authRepository,
+  name: r'authRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AuthRepositoryRef = ProviderRef<AuthRepository>;
+String _$storesRepositoryHash() => r'0118c515814002d9ca430983e79e9cdcdafd7089';
+
+/// See also [storesRepository].
+@ProviderFor(storesRepository)
+final storesRepositoryProvider = Provider<StoresRepository>.internal(
+  storesRepository,
+  name: r'storesRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$storesRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef StoresRepositoryRef = ProviderRef<StoresRepository>;
+String _$ordersRepositoryHash() => r'3656848aafceb89ecb81e309ed4cf2080262e267';
+
+/// See also [ordersRepository].
+@ProviderFor(ordersRepository)
+final ordersRepositoryProvider = Provider<OrdersRepository>.internal(
+  ordersRepository,
+  name: r'ordersRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$ordersRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef OrdersRepositoryRef = ProviderRef<OrdersRepository>;
+String _$productsRepositoryHash() =>
+    r'f3d25bc406a9ee636888145ad863f64e0857e079';
+
+/// See also [productsRepository].
+@ProviderFor(productsRepository)
+final productsRepositoryProvider = Provider<ProductsRepository>.internal(
+  productsRepository,
+  name: r'productsRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$productsRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ProductsRepositoryRef = ProviderRef<ProductsRepository>;
+String _$userRepositoryHash() => r'0258a6a5d1763660240196e092bac8c7a0049aaa';
+
+/// See also [userRepository].
+@ProviderFor(userRepository)
+final userRepositoryProvider = Provider<UserRepository>.internal(
+  userRepository,
+  name: r'userRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UserRepositoryRef = ProviderRef<UserRepository>;
+String _$storageRepositoryHash() => r'ac46e050d8c87ecafb6a56f9a852ba909525eda9';
+
+/// See also [storageRepository].
+@ProviderFor(storageRepository)
+final storageRepositoryProvider = Provider<StorageRepository>.internal(
+  storageRepository,
+  name: r'storageRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$storageRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef StorageRepositoryRef = ProviderRef<StorageRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
